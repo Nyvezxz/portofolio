f53e0d6d1b2f6a77f54fa987b3e3a25906aad7dd
Nyvezxz <glngpratama@gmail.com>
2026-06-13 06:10:23 +0700

    nvncsnva portfolio

---
+++ script.js
@@ -0,0 +1,85 @@
+console.log("Portfolio Loaded");
+
+// Efek muncul saat scroll nanti bisa ditambah di sini
+
+window.addEventListener("scroll", () => {
+
+    const nav = document.querySelector("nav");
+
+    if(window.scrollY > 50){
+
+    nav.style.background =
+    "rgba(15,23,42,.8)";
+
+    nav.style.boxShadow =
+    "0 10px 40px rgba(14,165,233,.15)";
+
+}else{
+
+    nav.style.background =
+    "rgba(255,255,255,.05)";
+
+    nav.style.boxShadow =
+    "none";
+}
+
+});
+
+const hiddenElements =
+document.querySelectorAll(".hidden");
+
+const observer =
+new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }else{
+            entry.target.classList.remove("show");
+        }
+
+    });
+
+}, {
+    threshold: 0.2
+});
+
+hiddenElements.forEach((el) => {
+    observer.observe(el);
+});
+
+const texts = [
+    "IT Support",
+    "Tech Enthusiast",
+    "Hardware Troubleshouter",
+    "Fresh Graduate"
+];
+
+let textIndex = 0;
+let charIndex = 0;
+
+function typeEffect() {
+    const typing = document.getElementById("typing");
+
+    if(charIndex < texts[textIndex].length){
+        typing.textContent += texts[textIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(typeEffect, 100);
+    }else{
+        setTimeout(() => {
+            typing.textContent = "";
+            charIndex = 0;
+            textIndex = (textIndex + 1) % texts.length;
+            typeEffect();
+        }, 2000);
+    }
+}
+
+typeEffect();
+
+function copyEmail(){
+    navigator.clipboard.writeText("banggalang1927@gmail.com");
+
+    alert("Email Berhasil Disalin");
+}
